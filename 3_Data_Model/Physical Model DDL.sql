fe36c6a5281f6a80a7155ee7721b2c0627578816
Surya <sssureshs@mail.yu.edu>
2024-04-25 17:35:55 -0400

    ETL Updates

--- 3_Data_Model/Physical Model DDL.sql
+++ 3_Data_Model/Physical Model DDL.sql
@@ -2,7 +2,7 @@ DROP SCHEMA IF EXISTS citibike_dw;
 CREATE SCHEMA citibike_dw;
 
 CREATE  TABLE citibike_dw.date_dim ( 
-	date_sk_id           INT    NOT NULL   PRIMARY KEY,
+	date_sk_id           INT    NOT NULL   PRIMARY KEY AUTO_INCREMENT,
 	`date`               DATE    NOT NULL   ,
 	year                 INT    NOT NULL   ,
 	quarter              INT    NOT NULL   ,
@@ -13,17 +13,17 @@ CREATE  TABLE citibike_dw.date_dim (
  );
 
 CREATE  TABLE citibike_dw.member_type_dim ( 
-	member_type_sk_id   INT    NOT NULL   PRIMARY KEY,
+	member_type_sk_id   INT    NOT NULL   PRIMARY KEY AUTO_INCREMENT,
 	member_type_desc     TEXT    NOT NULL   
  );
 
 CREATE  TABLE citibike_dw.rideable_type_dim ( 
-	rideable_type_sk_id  INT    NOT NULL   PRIMARY KEY,
+	rideable_type_sk_id  INT    NOT NULL   PRIMARY KEY AUTO_INCREMENT,
 	rideable_type_desc   TEXT    NOT NULL   
  );
 
 CREATE  TABLE citibike_dw.station_dim ( 
-	station_sk_id        INT    NOT NULL   PRIMARY KEY,
+	station_sk_id        INT    NOT NULL   PRIMARY KEY AUTO_INCREMENT,
 	station_id           INT    NOT NULL   ,
 	station_name         VARCHAR(100)    NOT NULL   ,
 	latitude             TEXT    NOT NULL   ,
@@ -31,7 +31,7 @@ CREATE  TABLE citibike_dw.station_dim (
  );
 
 CREATE  TABLE citibike_dw.weather_dim ( 
-	weather_sk_id        INT    NOT NULL   PRIMARY KEY,
+	weather_sk_id        INT    NOT NULL   PRIMARY KEY AUTO_INCREMENT,
 	weather_name         VARCHAR(100)    NOT NULL   
  );
  
